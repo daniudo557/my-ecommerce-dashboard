fc0185f9bc72e4fdedebe4c3e73b49609a4ea152
daniudo557 <daniel.macedo@unesp.br>
2020-04-08 19:26:13 -0300

    Create /pages and Analytics.js

---
+++ src/pages/Analytics.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Analytics = () => {
+  return (
+    <div>
+      <header>
+        <h1>Página de Analytics</h1>
+      </header>
+      <p>
+        Exemplo de Analytics :)
+      </p>
+    </div>
+  )
+}
+export default Analytics
