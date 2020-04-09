f1a5be113b676b001b53f20d982aaf4d3921ee0f
daniudo557 <daniel.macedo@unesp.br>
2020-04-08 22:34:28 -0300

    Update analytics container style

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Analytics = () => {
   return (
-    <div style={{ backgroundColor: 'red' }}>
+    <div style={{ backgroundColor: 'red', width: '100%', marginLeft: 64 }}>
       <header>
         <h1>Página de Analytics</h1>
       </header>
