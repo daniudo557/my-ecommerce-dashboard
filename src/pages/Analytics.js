e65463fbd00dc49a11412367d7733e39fc523188
daniudo557 <daniel.macedo@unesp.br>
2020-04-09 00:03:44 -0300

    Add simple echart graphics

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -1,11 +1,75 @@
 import React from 'react'
 import { Container, PurpleSquareContainer, PurpleSquare } from './styles'
+import ReactEcharts from 'echarts-for-react'
+
 const Analytics = () => {
+  const getOption = () => {
+    return {
+      title: {
+        text: 'aa'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['primeiro', 'segundo', 'terceiro']
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          boundaryGap: false,
+          data: ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: 'primeiro',
+          type: 'line',
+          stack: 'Total',
+          areaStyle: { normal: {} },
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: 'segundo',
+          type: 'line',
+          stack: 'Total',
+          areaStyle: { normal: {} },
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name: 'terceiro',
+          type: 'line',
+          stack: 'Total',
+          areaStyle: { normal: {} },
+          data: [150, 232, 201, 154, 190, 330, 410]
+        }
+      ]
+    }
+  }
   return (
     <Container>
       <div style={{ backgroundColor: 'blue' }}>
         <PurpleSquareContainer>
-          <PurpleSquare />
+          <ReactEcharts
+            option={getOption()}
+            style={{ height: '350px', width: '100%' }}
+          />
           <PurpleSquare />
           <PurpleSquare />
         </PurpleSquareContainer>
